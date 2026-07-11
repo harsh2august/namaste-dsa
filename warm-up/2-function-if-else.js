dbc0e0a5a2d0d2cd73e74092c97077e7e4fb5b99
Harsh Kumar <harshj7.net@gmail.com>
2026-07-11 19:13:48 +0530

    Add initial JavaScript functions and voter eligibility check; include .DS_Store in .gitignore

--- warm-up/2-function-if-else.js
+++ warm-up/2-function-if-else.js
@@ -1,4 +1,51 @@
+/*
 function printHelloWorld() {
     console.log("Welcome to Mumbai..")
 }
 printHelloWorld() //Welcome to Mumbai..
+
+function greet(name) {
+    console.log(`Hello ${name}`)
+}
+let x = "Virat Kohli"
+greet(x) //Hello Virat Kohli
+
+
+function sum(a, b) { //arguments
+    return a + b
+}
+const add = sum(3, 5) //parameters
+console.log("Sum is:", add)
+
+
+function square(num) {
+    return num * num
+}
+const result = square(99)
+console.log("Square is:", result) //Square is: 9801
+
+
+
+function checkVoter(age) {
+    if (age <= 0) {
+        console.log("Age cannot be negative or zero")
+    }
+    else if (age > 0 && age >= 18) {
+        console.log("You are eligible to vote")
+    } else {
+        console.log("You are not eligible to vote")
+    }
+}
+checkVoter(20) //You are eligible to vote
+*/
+
+function isEven(num) {
+    if (num % 2 === 0) {
+        console.log(`${num} is an even number`)
+    } else {
+        console.log(`${num} is an odd number`)
+    }
+}
+
+isEven(10) //10 is an even number
+isEven(15) //15 is an odd number
