d5bd4f6e719950e95e5f663a52c1ff0f908be75f
Harsh Kumar <harshj7.net@gmail.com>
2026-07-12 15:22:49 +0530

    Implement while loop to display incrementing messages

---
+++ warm-up/3-loops-01.js
@@ -0,0 +1,25 @@
+/*
+for (let i = 10; i > 0; i--) {
+    console.log(i)
+}
+
+function greet(i) {
+    console.log("Hello World " + i)
+}
+for (let i = 0; i < 10; i++) {
+    greet(i)
+}
+
+let marks = [80, 72, 95, 73, 70, 94, 99, 88]
+for (let i = 0; i < marks.length; i++) {
+    marks[i] % 2 == 0 && console.log(marks[i])
+}
+console.log(marks.filter((i) => i % 2 == 0))
+    */
+
+// while loop
+let i = 1
+while (i <= 10) {
+    console.log("Harsh has " + i)
+    i++
+}
